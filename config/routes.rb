b52bc6ee7227646a7559f8187460f9b3245ed522
darbyod <165102225+darbyod@users.noreply.github.com>
2024-05-07 02:57:59 +0000

    photos index working

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ get("/users",{:controller => "users", :action => "index"})
 
 get("/users/:path_username",{:controller => "users", :action => "show"})
 
+get("/photos",{:controller => "photos", :action => "index"})
+
+
 
 
 end
