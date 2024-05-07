3fd1e7b0b9c8d6b0ed1e9de9a95470fc43a48751
darbyod <165102225+darbyod@users.noreply.github.com>
2024-05-07 03:23:57 +0000

    photo delete working

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ get("/photos",{:controller => "photos", :action => "index"})
 
 get("/photos/:path_id",{:controller => "photos", :action => "show"})
 
-
+get("/delete_photo/:path_id", {:controller => "photos", :action => "delete"})
 
 
 end
